ec19e7591dff2db0c0dbacba8f9e1b5b98adb92b
Aditto <avikd906@proton.me>
2025-01-12 01:13:24 +0530

    completed footer section. Working On Navbar

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,6 +7,8 @@ import { router } from './Routes/Routes.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router}></RouterProvider>
+    <div className='max-w-screen-2xl mx-auto'>
+      <RouterProvider router={router}></RouterProvider>
+    </div>
   </StrictMode>,
 )
